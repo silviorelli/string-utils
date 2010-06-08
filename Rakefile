c10dd50f9af214e28b12edcfebc6f5feac533f16
Silvio Relli <silvio@relli.org>
2010-06-08 19:30:18 +0200

    Initial commit

---
+++ Rakefile
@@ -0,0 +1,14 @@
+require 'rubygems'
+require 'rake'
+require 'echoe'
+
+Echoe.new('string-utils', '0.1.0') do |p|
+  p.description    = "This small extension enables string sanitization in models and controller. Provides also a function for making url friendly strings removing all unwanted characters."
+  p.url            = "http://github.com/silviorelli/string-utils"
+  p.author         = "Silvio Relli"
+  p.email          = "silvio@relli.org"
+  p.ignore_pattern = ["tmp/*", "script/*"]
+  p.development_dependencies = []
+end
+
+Dir["#{File.dirname(__FILE__)}/tasks/*.rake"].sort.each { |ext| load ext }
---
+++ lib/string-utils.rb
@@ -0,0 +1,38 @@
+# Silvio Relli's string_utils gem - released under the MIT License
+class String
+  
+  # Workaround for sanitize method not availabe in controllers, models and libs.
+  # Now you can call "mystryng".sanitize everywere.
+  def sanitize
+    ActionController::Base.helpers.sanitize(self)
+  end
+  
+  # Ensures the string is sanitized and html safe,
+  # so it won't be escaped by xss protection and erubis in rails >=2.3.6 and >=3.0.
+  def san
+    self.sanitize
+  end
+  
+  # Just a shortcut to force a string to be html safe.
+  # Use with caution.
+  def hs
+    self.html_safe
+  end
+  
+  # Makes strings be url-friendly
+  def urlify
+    str = self.downcase.gsub(/ /,'-')
+    str.gsub!(/À|à|á|Â|â|Ã|ã|Ä|ä/i,'a') 
+    str.gsub!(/È|è|É|é|Ê|ê|Ë|ë/i,'e')
+    str.gsub!(/Ì|ì|í|Î|î/i,'i')
+    str.gsub!(/Ò|ò|ó|Ô|ô|Õ|õ|Ö|ö/i,'o')
+    str.gsub!(/Ù|ù|ú|Û|û|Ü|ü/i,'u')
+    str.gsub!(/ç/i,'c')
+    str.gsub!(/œ/i,'oe')
+    str.gsub!(/ß/i,'ss')
+    str.gsub!(/[^a-z0-9\-]+/i, '')
+    str
+  end
+  
+end
+
